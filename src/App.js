6c66e84376d18fb7bed7ddc71d14777333ddf558
user0 <igorbox1976@gmail.com>
2024-04-14 20:12:57 +0300

    Add btn 4

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className='container'>
-      {['Оди', 'Два', 'Три'].map(b => 
+      {['Оди', 'Два', 'Три', 'Четыре'].map(b => 
         <div className='btn-container'>
           <button className='btn'><span>{b}</span></button>
+
         </div>
       )}
     </div>
